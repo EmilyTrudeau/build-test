cde1acbd057f00d58b2eb32f19a131e758e8ebc6
Emily Trudeau <emilytrudeau@flywheel.io>
2020-12-10 15:07:07 -0600

    removed pages

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     'Getting Started': ['GettingStarted', 'QuickStart'],
-    'Work with Data': ['FindingData', 'CreateObjects', 'SettingObjectMetadata'],
+    'Work with Data': ['FindingData', 'CreateObjects'],
     'Data Model':['hierarchy', 'permissions', 'custom_roles', 'containers'],
     'Flywheel Views':['views'],
     'SDK Gears':['sdk_gears'],
